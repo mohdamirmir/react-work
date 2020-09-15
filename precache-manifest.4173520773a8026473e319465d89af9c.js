3404f7d8ea669efd68f784d2b0bce801dc38138b
aamirmir111 <aamir.mir111@gmail.com>
2020-09-15 11:08:59 +0400

    Updates

---
+++ precache-manifest.4173520773a8026473e319465d89af9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "639adfde9465970146554f91caa63df9",
+    "url": "/react-work/index.html"
+  },
+  {
+    "revision": "d44ba9c52266cfb9b30f",
+    "url": "/react-work/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8d21629c41174ceec1c8",
+    "url": "/react-work/static/js/2.44b8750f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-work/static/js/2.44b8750f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d44ba9c52266cfb9b30f",
+    "url": "/react-work/static/js/main.b18b9632.chunk.js"
+  },
+  {
+    "revision": "ae94055c16d841911574",
+    "url": "/react-work/static/js/runtime-main.3f127c82.js"
+  }
+]);
